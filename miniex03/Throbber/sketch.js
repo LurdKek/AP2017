5b85311ccd4e9bc9a9c2c031cf7c72dbdd42fe57
LurdKek <casperlerka@gmail.com>
2017-02-25 03:19:44 +0100

    Update sketch.js

--- miniex03/Throbber/sketch.js
+++ miniex03/Throbber/sketch.js
@@ -1,6 +1,6 @@
 //rad = radius; radAdd = adding to radius
 var radAdd = 1;
-var rad = 100;
+var rad = 20;
 
 function setup() {
     console.log("is working");
@@ -10,11 +10,16 @@ function setup() {
 }
 
 function draw() {
+    textSize(50);
+    fill(100, 20);
+    text("IF U PUSH ME I GET NERVOUS", 50, 50);
+    
     //var tnum = map(rad, 100, 300, 30, 60);
     var tnum = 75
     
     if(mouseIsPressed){
         tnum = map(rad, 100, 200, 75, 150); //turn on by mouseclick
+        
     }
     
     fill(10, 10);
@@ -29,18 +34,22 @@ function drawThrobber(num) {
     translate(width/2, height/2);
     
     var cir = 1440/num*(frameCount%num);
-    var r = map(rad, 100, 200, 50, 220);
-    var g = map(rad, 100, 200, 200, 50);
-    var b = map(rad, 100, 200, 0, 50);
+    var r = map(rad, 25, 200, 50, 220);
+    var g = map(rad, 25, 200, 200, 50);
+    var b = map(rad, 25, 200, 0, 50);
+    var sizeW = 20;
+    var sizeH = 20;
+    sizeW = map(rad, 35, 200, 50, 150);
+    sizeH = map(rad, 35, 200, 50, 150);
     
     rad = rad+radAdd;
     rotate(radians(cir));
     noStroke();
     fill(r, g, b);
-    arc(rad, 35, 150, 150, PI, PI+QUARTER_PI);
+    arc(rad, 35, sizeW, sizeH, PI, PI+QUARTER_PI);
     if (rad >= 200) {
         radAdd = -1
-    } else if(rad <= 100) {
+    } else if(rad <= 25) {
         radAdd = +1
     }
     
