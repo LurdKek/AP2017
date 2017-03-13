3c584be78fa7b7882830b303f0f8cc200795ca50
LurdKek <casperlerka@gmail.com>
2017-03-13 01:50:04 +0100

    Add files via upload

---
+++ Miniex05/SimpleRiTa/sketch.js
@@ -0,0 +1,34 @@
+var input;
+var button;
+
+function setup() {
+    noCanvas();
+    lexicon = new RiLexicon();
+    input = createInput('it was a dark and stormy night.');
+    button = createButton("submit");    
+    input.changed(processRita);
+    button.mousePressed(processRita);
+    input.size(300);
+}
+
+function processRita() {
+    
+    var s = input.value();
+    var rs = new RiString(s);
+    var words = rs.words();
+    var pos = rs.pos();
+    console.log(words);
+    console.log(pos);
+    
+    var output = "";
+    for (var i = 0; i < words.length; i++) {
+        if (/nn.*/.test(pos[i])) {
+            output += lexicon.randomWord(pos[i]);  //give me another noun via Rita Library
+        } else{
+         output += words[i];   
+        }
+        
+        output += " ";
+    }    
+    createP(output);
+}
